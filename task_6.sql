49c8c1b8cce8454db10cd27625f6ecb3c890b6f6
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-10-18 16:41:31 +0100

    .\task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -9,14 +9,14 @@ mydb = (
 
 mycursor = mydb.cursor()
 
-sql = "INSERT INTO customer(customer_id, customer_name, email, address) VALUES(%d, %s, %s, %s)"
-data = [
-    (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
-    (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
-    (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.")
-    ]
+ sql = """
+    INSERT INTO Customers (customer_id, customer_name, email, address) VALUES 
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+    """
 
-mycursor.executemany(sql, data)
+mycursor.execute(sql)
 
 mydb.commit()
 
