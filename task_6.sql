a0781646b509301943ff7047bec3f35935e69be4
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-10-18 16:50:07 +0100

    .\task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,24 @@
--- Inserting multiple records into the Customers table
+import mysql.connector
 
-INSERT INTO Customers (customer_id, customer_name, email, address) VALUES 
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+mydb = (
+    host = "localhost"
+    user= "root"
+    password = "1234"
+    database = "alx_book_store"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO customer(customer_id, customer_name, email, address) VALUES(%d, %s, %s, %s)"
+data = [
+    (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave."),
+    (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
+    (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.")
+    ]
+
+mycursor.executemany(sql, data)
+
+-- mydb.commit()
+
+mycursor.Close()
+mydb.close()
