1df6ec3f9fc859d600dab4732b7edd5a0c13c53e
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-10-18 16:31:19 +0100

    .\task_6.sql

---
+++ task_6.sql
@@ -0,0 +1,22 @@
+import mysql.connector
+
+mydb = (
+    host = "localhost"
+    user= "root"
+    password = "1234"
+    database = "alx_book_store"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO customer(customer_id, customer_name, email, address) VALUES(%d, %s, %s, %s)"
+data = [
+    (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave.")
+    (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave.")
+    (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.")
+    ]
+
+mycursor.execute(sql, data)
+
+mycursor.Close()
+mydb.close()
