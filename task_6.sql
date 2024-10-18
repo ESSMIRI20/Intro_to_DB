d2a930725fe8b5ad6e73b5af96db45fd1a5023a5
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-10-18 16:44:38 +0100

    .\task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,24 +1,6 @@
-import mysql.connector
+-- Inserting multiple records into the Customers table
 
-mydb = (
-    host = "localhost"
-    user= "root"
-    password = "1234"
-    database = "alx_book_store"
-)
-
-mycursor = mydb.cursor()
-
- sql = """
-    INSERT INTO Customers (customer_id, customer_name, email, address) VALUES 
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
-    """
-
-mycursor.execute(sql)
-
-mydb.commit()
-
-mycursor.Close()
-mydb.close()
+INSERT INTO Customers (customer_id, customer_name, email, address) VALUES 
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
