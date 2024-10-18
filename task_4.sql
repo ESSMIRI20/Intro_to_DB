639c83c2e48be3052a68db34e31f38292d57364c
ESSMIRI20 <OSSAMA_ESSMIRI@OSSAMA.com>
2024-10-18 16:05:47 +0100

    .\task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb = (
+    host = "localhost",
+    user = "root",
+    password = "1234",
+    database = "alx_book_store"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("DESCRIBE BOOKS")
+books_description = mycursor.fetchall()
+
+for i in books_description:
+    print(i)
+
+mycursor.close()
+mydb.close()
